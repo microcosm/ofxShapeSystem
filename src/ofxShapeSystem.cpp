7909085084ea5849954e3c5d1ac3d8871c1ba0c7
Andrew McWilliams <andy@jahya.net>
2015-05-27 01:06:20 -0400

    Adds ability to transform entire system

--- src/ofxShapeSystem.cpp
+++ src/ofxShapeSystem.cpp
@@ -5,7 +5,9 @@
 //https://github.com/jasonmcdermott/ofxForums/tree/master/vertexArrayGradientShapes
 
 void ofxShapeSystem::setup() {
-    
+    setPosition(ofVec3f(0, 0, 0));
+    setRotation(ofVec3f(0, 0, 0));
+    setScale(ofVec3f(1, 1, 1));
 }
 
 void ofxShapeSystem::add(ofxShape &shape) {
@@ -21,11 +23,106 @@ void ofxShapeSystem::update() {
 void ofxShapeSystem::draw() {
     glEnableClientState(GL_COLOR_ARRAY);
     glEnableClientState(GL_VERTEX_ARRAY);
+
+    ofPushMatrix();
+    ofTranslate(position.x, position.y, position.z);
+    ofRotateX(rotation.x);
+    ofRotateY(rotation.y);
+    ofRotateZ(rotation.z);
+    ofScale(scale.x, scale.y, scale.z);
+
     for(int i = 0; i < shapes.size(); i++) {
         shapes[i]->draw();
     }
+
+    ofPopMatrix();
+
     glDisableClientState(GL_VERTEX_ARRAY);
     glDisableClientState(GL_COLOR_ARRAY);
+}
+
+void ofxShapeSystem::setPosition(ofVec3f position_) {
+    position.set(position_);
+}
+
+void ofxShapeSystem::setRotation(ofVec3f rotation_) {
+    rotation.set(rotation_);
+}
+
+void ofxShapeSystem::setScale(ofVec3f scale_) {
+    scale.set(scale_);
+}
+
+void ofxShapeSystem::rotateX(float degrees) {
+    rotation.x = degrees;
+}
+
+void ofxShapeSystem::rotateY(float degrees) {
+    rotation.y = degrees;
+}
+
+void ofxShapeSystem::rotateZ(float degrees) {
+    rotation.z = degrees;
+}
+
+void ofxShapeSystem::incrementRotateX(float amount) {
+    rotation.x += amount;
+}
+
+void ofxShapeSystem::incrementRotateY(float amount) {
+    rotation.y += amount;
+}
+
+void ofxShapeSystem::incrementRotateZ(float amount) {
+    rotation.z += amount;
+}
+
+void ofxShapeSystem::positionX(float positionX) {
+    position.x = positionX;
+}
+
+void ofxShapeSystem::positionY(float positionY) {
+    position.y = positionY;
+}
+
+void ofxShapeSystem::positionZ(float positionZ) {
+    position.z = positionZ;
+}
+
+void ofxShapeSystem::incrementPositionX(float amount) {
+    position.x += amount;
+}
+
+void ofxShapeSystem::incrementPositionY(float amount) {
+    position.y += amount;
+}
+
+void ofxShapeSystem::incrementPositionZ(float amount) {
+    position.z += amount;
+}
+
+void ofxShapeSystem::scaleX(float scaleX) {
+    scale.x = scaleX;
+}
+
+void ofxShapeSystem::scaleY(float scaleY) {
+    scale.y = scaleY;
+}
+
+void ofxShapeSystem::scaleZ(float scaleZ) {
+    scale.z = scaleZ;
+}
+
+void ofxShapeSystem::incrementScaleX(float amount) {
+    scale.x += amount;
+}
+
+void ofxShapeSystem::incrementScaleY(float amount) {
+    scale.y += amount;
+}
+
+void ofxShapeSystem::incrementScaleZ(float amount) {
+    scale.z += amount;
 }
 
 vector <ofxShape*> ofxShapeSystem::getShapes() {
--- src/ofxShapeSystem.h
+++ src/ofxShapeSystem.h
@@ -15,8 +15,32 @@ public:
     void add(ofxShape &shape);
     void update();
     void draw();
+
+    void setPosition(ofVec3f position_);
+    void setRotation(ofVec3f rotation_);
+    void setScale(ofVec3f rotation_);
+    void rotateX(float degrees);
+    void rotateY(float degrees);
+    void rotateZ(float degrees);
+    void incrementRotateX(float amount);
+    void incrementRotateY(float amount);
+    void incrementRotateZ(float amount);
+    void positionX(float positionX);
+    void positionY(float positionY);
+    void positionZ(float positionZ);
+    void incrementPositionX(float amount);
+    void incrementPositionY(float amount);
+    void incrementPositionZ(float amount);
+    void scaleX(float scaleX);
+    void scaleY(float scaleY);
+    void scaleZ(float scaleZ);
+    void incrementScaleX(float amount);
+    void incrementScaleY(float amount);
+    void incrementScaleZ(float amount);
+
     vector <ofxShape*> getShapes();
-    
+
 private:
     vector <ofxShape*> shapes;
+    ofVec3f position, rotation, scale;
 };
